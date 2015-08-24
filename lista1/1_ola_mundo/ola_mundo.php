d19956ac71a204abd4139618075f4012ad8e1bb8
Everaldo Gomes <everaldo.gomes@gmail.com>
2015-08-24 10:46:46 -0300

    resolução da lista 1 - oficina programação

---
+++ lista1/1_ola_mundo/ola_mundo.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Everaldo Gomes - everaldo.gomes@pucpr.br
+ * 23/08/2015
+ *
+ * Resolução de Exercícios
+ *
+ * 1. Olá Mundo
+ *
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Olá Mundo</title>
+</head>
+<body>
+<h1><?php echo "Olá Mundo"; ?></h1>
+</body>
+</html>
---
+++ lista1/2_ola/ola.php
@@ -0,0 +1,35 @@
+<?php
+/* Everaldo Gomes - everaldo.gomes@pucpr.br
+ * 23/08/2015
+ *
+ * 2. Criar uma página index.php, com um formulário
+ * com um único campo do tipo texto, com alvo para
+ *  a página ola.php, utilizando o método de submissão
+ * GET. A página ola.php deve imprimir uma mensagem
+ * “Olá, $nome”, onde
+ * $nome é o valor submetido no formulário.
+ *
+ *
+ * arquivo: ola.php
+ *
+ */
+
+function cumprimenta(){
+  $nome = $_GET["nome"];
+  if(! empty($nome)){
+    echo "Olá, $nome";
+  }
+}
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Olá</title>
+</head>
+<body>
+  <?php cumprimenta(); ?>
+</body>
+</html>
